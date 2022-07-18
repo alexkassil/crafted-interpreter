55caadf16c3920e300953cebb5db74a434e4977d
Alex Kassil <afk@fb.com>
2022-07-18 10:36:18 -0700

    Add error report function with the power of eta reduction

--- lox_interpreter/lox/lox.ml
+++ lox_interpreter/lox/lox.ml
@@ -2,21 +2,27 @@
 (* open Stdio *)
 open Core
 
-let eval _ = "hi "
+(* TODO: Make this return a Result type*)
+let eval source:string = source
 
 let run_file filename =
   let file = In_channel.create filename in
   let input_lines = In_channel.input_lines file in
-  List.iter input_lines ~f:print_endline;
+  let input = String.concat ~sep:"\n" input_lines in
+  print_endline input;
   In_channel.close file;
-  let _ = eval input_lines in
+  let _ = eval input in
   ()
 
+let report = printf "[line %d] Error%s: %s"
+
+let lox_error line =
+  report line ""
 
 let rec repl () =
   printf "> %!";
   (* read eval print loop *)
-  repl @@ print_endline @@ eval [In_channel.(input_line_exn stdin)]
+  repl @@ print_endline @@ eval In_channel.(input_line_exn stdin)
 
 let () =
   let args = Sys.get_argv () in
